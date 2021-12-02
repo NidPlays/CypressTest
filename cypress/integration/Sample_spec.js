84fd7070734e52478ac3c434f20a523c03fad737
Nid <nidgan007@gmail.com>
2021-12-02 21:53:57 +0530

    Update

--- cypress/integration/Sample_spec.js
+++ cypress/integration/Sample_spec.js
@@ -3,93 +3,106 @@
 describe('My First Test', () => {
     it('finds the content "type"', () => {
         //visits the site provided
+
         cy.visit('https://example.cypress.io')
         //clicks the first element which has 'type' in its data
         cy.contains('type').click()
         //assertion for url to include /commands/actions
         cy.url().should('include', '/commands/actions')
-
-        //next part
-        //goes to element containing class .action-email
-        //then types an email and checks
-        cy.get('.action-email')
-            .type('nid@myemaildomain.com')
-            //checks
-            //.should('have.value', 'fake@email.com')
-            .should('have.value', 'nid@myemaildomain.com')
-
-
-
-        cy.get('.action-disabled')
-            .type('disabled error checking', { force: true })
-            //will show error when force is not enabled, user cannot go through when button is disabled normally
-            //.type('disabled error checking')
-            .should('have.value', 'disabled error checking')
-            //clear is chained function which is used to clear the input box then checks
-            //.clear() will fail again because its disabkes
-            .clear({force:true})
-            .should('have.value','')
+        //
+        // //next part
+        // //goes to element containing class .action-email
+        // //then types an email and checks
+        // cy.get('.action-email')
+        //     .type('nid@myemaildomain.com')
+        //     //checks
+        //     //.should('have.value', 'fake@email.com')
+        //     .should('have.value', 'nid@myemaildomain.com')
+        //
+        //
+        //
+        // cy.get('.action-disabled')
+        //     .type('disabled error checking', { force: true })
+        //     //will show error when force is not enabled, user cannot go through when button is disabled normally
+        //     //.type('disabled error checking')
+        //     .should('have.value', 'disabled error checking')
+        //     //clear is chained function which is used to clear the input box then checks
+        //     //.clear() will fail again because its disabkes
+        //     .clear({force:true})
+        //     .should('have.value','')
 
 
         //using #idname would be the id element in html .classname would be for class
-        /*
-        cy.get('#action-canvas').click()
 
-        //has inbuilt positions
-        //Valid positions are topLeft, top, topRight, left, center, right, bottomLeft, bottom, and bottomRight.
-        cy.get('#action-canvas').click('topLeft')
-        cy.get('#action-canvas').click('top')
-        cy.get('#action-canvas').click('topRight')
-        cy.get('#action-canvas').click('left')
-        cy.get('#action-canvas').click('right')
-        cy.get('#action-canvas').click('bottomLeft')
-        cy.get('#action-canvas').click('bottom')
-        cy.get('#action-canvas').click('bottomRight')
+        // cy.get('#action-canvas').click()
+        //
+        // //has inbuilt positions
+        // //Valid positions are topLeft, top, topRight, left, center, right, bottomLeft, bottom, and bottomRight.
+        // cy.get('#action-canvas').click('topLeft')
+        // cy.get('#action-canvas').click('top')
+        // cy.get('#action-canvas').click('topRight')
+        // cy.get('#action-canvas').click('left')
+        // cy.get('#action-canvas').click('right')
+        // cy.get('#action-canvas').click('bottomLeft')
+        // cy.get('#action-canvas').click('bottom')
+        // cy.get('#action-canvas').click('bottomRight')
 
         // .click() accepts a an x and y coordinate
         // that controls where the click occurs
         cy.get('#action-canvas')
-            .click(80, 75)
-            .click(170, 75)
-            .click(80, 165)
-            .click(100, 185)
-            .click(125, 190)
-            .click(150, 185)
-            .click(170, 165)
+            .click(40, 180)
+            .click(40, 160)
+            .click(40, 140)
+            .click(40, 120)
+            .click(40, 100)
+
+            .click(50,120)
+            .click(55,130)
+            .click(60,140)
+            .click(65,150)
+            .click(70,160)
+
+            .click(80, 180)
+            .click(80, 160)
+            .click(80, 140)
+            .click(80, 120)
+            .click(80, 100)
 
 
-         */
 
 
-        // click multiple elements by passing multiple: true
-        //cy.get('.action-labels>.label').click({ multiple: true })
-
-        // Ignore error checking prior to clicking
-        cy.get('.action-opacity>.btn').click({ force: true })
 
 
-        //checking form boxes
-        cy.get('.action-checkboxes [type="checkbox"]').not('[disabled]')
-            .check().should('be.checked')
 
-        cy.get('.action-radios [type="radio"]').not('[disabled]')
-            .check().should('be.checked')
-
-        // .check() accepts a value argument
-        cy.get('.action-radios [type="radio"]')
-            .check('radio1').should('be.checked')
-
-        // .check() accepts an array of values
-        cy.get('.action-multiple-checkboxes [type="checkbox"]')
-            .check(['checkbox1', 'checkbox2']).should('be.checked')
-
-        // Ignore error checking prior to checking
-        cy.get('.action-checkboxes [disabled]')
-            .check({ force: true }).should('be.checked')
-
-        cy.get('.action-radios [type="radio"]')
-            .check('radio2', { force: true }).should('be.checked')
-            //.check('radio2', { force: true }).should('be.checked')
-
+        // // click multiple elements by passing multiple: true
+        // //cy.get('.action-labels>.label').click({ multiple: true })
+        //
+        // // Ignore error checking prior to clicking
+        // cy.get('.action-opacity>.btn').click({ force: true })
+        //
+        //
+        // //checking form boxes
+        // cy.get('.action-checkboxes [type="checkbox"]').not('[disabled]')
+        //     .check().should('be.checked')
+        //
+        // cy.get('.action-radios [type="radio"]').not('[disabled]')
+        //     .check().should('be.checked')
+        //
+        // // .check() accepts a value argument
+        // cy.get('.action-radios [type="radio"]')
+        //     .check('radio1').should('be.checked')
+        //
+        // // .check() accepts an array of values
+        // cy.get('.action-multiple-checkboxes [type="checkbox"]')
+        //     .check(['checkbox1', 'checkbox2']).should('be.checked')
+        //
+        // // Ignore error checking prior to checking
+        // cy.get('.action-checkboxes [disabled]')
+        //     .check({ force: true }).should('be.checked')
+        //
+        // cy.get('.action-radios [type="radio"]')
+        //     .check('radio2', { force: true }).should('be.checked')
+        //     //.check('radio2', { force: true }).should('be.checked')
+        //
     })
 })
