67c0235bfcaab73871c8337dd7a91e871547a7c3
Nid <nidgan007@gmail.com>
2021-12-04 00:18:10 +0530

    pass for gh-pages

--- cypress/integration/Sample_spec.js
+++ cypress/integration/Sample_spec.js
@@ -13,7 +13,7 @@ describe('Test with explain', () => {
 
 
         //clicks the first element which has 'type' in its data
-        cy.contains('willfailforgithubci').click()
+        cy.contains('type').click()
         //assertion for url to include /commands/actions
         cy.url().should('include', '/commands/actions')
 
